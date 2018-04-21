30eac1f0d9be084adc72a3952ae26ea23e01ba0b
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2018-04-21 08:58:55 +0200

    initial project

---
+++ src/main/java/pl/codilingus/library/LibraryApplication.java
@@ -0,0 +1,12 @@
+package pl.codilingus.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+}
