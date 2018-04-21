ad5ca01319a04a6f4076889e69bd8e288602b2b3
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2018-04-21 09:27:07 +0200

    initial class user

---
+++ src/main/java/pl/codilingus/library/User.java
@@ -0,0 +1,32 @@
+package pl.codilingus.library;
+
+public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getUserId(){
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
