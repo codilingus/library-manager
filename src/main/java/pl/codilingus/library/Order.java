31d1d54b611f1892e9d78ef107e79fcefd112812
Ewelina <evelss005@gmail.com>
2018-04-21 11:17:12 +0200

    add order class

---
+++ src/main/java/pl/codilingus/library/Order.java
@@ -0,0 +1,60 @@
+package pl.codilingus.library;
+
+import java.time.LocalDate;
+
+public class Order {
+    private int id;
+    private User user;
+    private Book borrowedBook;
+    private LocalDate dateOfBorrow;
+    private LocalDate dateToReturn;
+
+    public Order(User user, Book borrowedBook, LocalDate dateOfBorrow, LocalDate dateToReturn) {
+        this.user = user;
+        this.borrowedBook = borrowedBook;
+        this.dateOfBorrow = dateOfBorrow;
+        this.dateToReturn = dateToReturn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBorrowedBook() {
+        return borrowedBook;
+    }
+
+    public void setBorrowedBook(Book borrowedBook) {
+        this.borrowedBook = borrowedBook;
+    }
+
+    public LocalDate getDateOfBorrow() {
+        return dateOfBorrow;
+    }
+
+    public void setDateOfBorrow(LocalDate dateOfBorrow) {
+        this.dateOfBorrow = dateOfBorrow;
+    }
+
+    public LocalDate getDateToReturn() {
+        return dateToReturn;
+    }
+
+    public void setDateToReturn(LocalDate dateToReturn) {
+        this.dateToReturn = dateToReturn;
+    }
+
+    public boolean isAfterDeadline() {
+        LocalDate now = LocalDate.now();
+        return now.isAfter(this.dateOfBorrow);
+    }
+}
+
