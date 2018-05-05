8a6a50619a56ee74f4ea570ead9732bc9c6efdba
Ewelina <evelss005@gmail.com>
2018-05-05 12:50:21 +0200

    changes to orders validation

--- src/main/java/pl/codilingus/library/validation/OrderValidator.java
+++ src/main/java/pl/codilingus/library/validation/OrderValidator.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import pl.codilingus.library.Book;
 import pl.codilingus.library.Order;
 import pl.codilingus.library.controllers.OrderController;
+import pl.codilingus.library.repositories.OrderRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,10 +11,9 @@ import java.util.stream.Collectors;
 public class OrderValidator {
 
     @Autowired
-    private OrderController orderController;
+    private OrderRepository orderRepository;
 
-    private boolean validateOrderingMonth(Order newOrder){
-        List<Order> listOfOrders = orderController.getAllOrders();
+    private boolean validateOrderingMonth(Order newOrder, List<Order> listOfOrders){
         return listOfOrders.stream()
                 .filter(order -> order.getUser().getId() == newOrder.getUser().getId())
                 .filter(order -> order.getBorrowedBook().getId() == newOrder.getBorrowedBook().getId())
@@ -21,25 +21,24 @@ public class OrderValidator {
                 .collect(Collectors.toList()).isEmpty();
     }
 
-    private boolean validateBorrowedBook(Order newOrder){
-        List<Order> listOfOrders = orderController.getAllOrders();
+    private boolean validateBorrowedBook(Order newOrder, List<Order> listOfOrders){
         return listOfOrders.stream()
                 .filter(order -> order.getBorrowedBook().getId() == newOrder.getBorrowedBook().getId())
                 .filter(order -> order.getDateOfReturn() == null)
                 .collect(Collectors.toList()).isEmpty();
     }
 
-    private boolean validateAuthor(Order newOrder){
-        List<Order> listOfOrders = orderController.getAllOrders();
+    private boolean validateAuthor(Order newOrder, List<Order> listOfOrders){
         return listOfOrders.stream()
                 .filter(order -> order.getBorrowedBook().getAuthor().equals(newOrder.getBorrowedBook().getAuthor()))
                 .collect(Collectors.toList()).isEmpty();
     }
 
     public boolean validationMechanism(Order newOrder){
-        return validateOrderingMonth(newOrder)
-                && validateBorrowedBook(newOrder)
-                && validateAuthor(newOrder);
+        List<Order> listOfOrders = orderRepository.getAllOrders();
+        return validateOrderingMonth(newOrder, listOfOrders)
+                && validateBorrowedBook(newOrder, listOfOrders)
+                && validateAuthor(newOrder, listOfOrders);
     }
 
 }
